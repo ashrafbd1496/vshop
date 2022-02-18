81ad17362534d02eb8b36ee21f38865b9c15a39f
ashrafbd1496 <asraf1496@gmail.com>
2022-02-18 12:33:48 +0600

    added carbon field checkbox options to show hide banner section options

--- inc/carbon-options.php
+++ inc/carbon-options.php
@@ -14,7 +14,14 @@ function crb_attach_theme_options() {
 
     Container::make( 'theme_options', __( 'Carbon Options' ) )
         ->add_fields( array(
+            Field::make( 'checkbox', 'crb_show_content', 'Show content' ),
             Field::make( 'complex', 'crb_banners', 'Banners' )
+                 ->set_conditional_logic( array(
+                        array(
+                            'field' => 'crb_show_content',
+                            'value' => true,
+                        )
+                        ), )
                 ->set_layout( 'tabbed-horizontal' )
                 ->add_fields( array(
                     Field::make( 'text', 'b_item_title', 'Banner_item_title' )
@@ -23,19 +30,16 @@ function crb_attach_theme_options() {
                     Field::make( 'icon', 'b_item_icon', 'Banner Item Icon' )
                     ->set_default_value('fa-truck'),
                     Field::make( 'text', 'b_item_des', 'Banner Item Description' ),
+                    Field::make( 'separator', 'crb_separator', __( 'Separator' ) )
                 ) ),
-        ) );
+        ) )->add_tab( __( 'Notification' ), array(
+        Field::make( 'text', 'crb_email', __( 'Notification Email' ) ),
+        Field::make( 'text', 'crb_phone', __( 'Phone Number' ) ),
+    ) );
 
+    
 
-
-
-
-
-
-
-
-
-        
+   
 }
 
 
